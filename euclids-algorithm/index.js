41c60c21db4f2fad3b589e88d5f07b9f1df3af31
js-jslog <jhs4jbs@hotmail.co.uk>
2019-07-20 18:54:03 +0000

    Simplify logic and readability

--- euclids-algorithm/index.js
+++ euclids-algorithm/index.js
@@ -1,14 +1,9 @@
-const performEuclidsStep = function performEuclidsStep(smallInput, largeInput) {
-  const remainder = largeInput % smallInput;
-  return [remainder, smallInput];
-};
-
 const findCommonDivisor = function findCommonDivisor(smallInput, largeInput) {
-  const newPair = performEuclidsStep(smallInput, largeInput);
-  if (newPair[0] === 0) {
-    return newPair[1];
-  }
-  return findCommonDivisor(newPair[0], newPair[1]);
+  const remainder = largeInput % smallInput;
+
+  if (remainder === 0) return smallInput;
+
+  return findCommonDivisor(remainder, smallInput);
 };
 
 module.exports = { findCommonDivisor };
